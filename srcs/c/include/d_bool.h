7e4f339d4af0f3d5265b94063bc6ca7657c7394a
Quentin PEREZ <qperez42@gmail.com>
2013-08-30 11:33:04 +0200

    [MAKE_LIB]
    Add include directory (root include) and update make_lib for this

---
+++ srcs/c/include/d_bool.h
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   d_bool.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qperez <qperez42@gmail.com>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2013/08/30 11:28:15 by qperez            #+#    #+#             */
+/*   Updated: 2013/08/30 11:29:01 by qperez           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** <This file contains boolean type>
+** Copyright (C) <2013>  Quentin Perez <qperez42@gmail.com>
+**
+** This file is part of 42-toolkit.
+**
+** 42-toolkit is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef D_BOOL_H
+# define D_BOOL_H
+
+# define __bool_true_false_are_defined	1
+
+# ifndef __cplusplus
+#  define bool	_Bool
+# endif
+
+# define false	0
+# define true	1
+
+#endif
