5ced9fdb9ce38ead767be5f2de747605d0366c03
Quentin PEREZ <qperez42@gmail.com>
2013-10-08 11:40:18 +0200

    [F_FREE]
    Add uf_free_tab

---
+++ srcs/libc/f_memory/src/f_free.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   f_free.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qperez <qperez42@gmail.com>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2013/10/08 11:37:57 by qperez            #+#    #+#             */
+/*   Updated: 2013/10/08 11:40:02 by qperez           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** <This file contains all f_free function>
+** Copyright (C) <2013>  Quentin Perez <qperez42@gmail.com>
+**
+** This file is part of 42-toolkit.
+**
+** 42-toolkit is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdlib.h>
+#include <t_types.h>
+
+void	uf_free_tab(void **tab)
+{
+	ui	i;
+
+	i = 0;
+	while (tab[i] != NULL)
+	{
+		free(tab[i]);
+		i = i + 1;
+	}
+	free(tab);
+}
