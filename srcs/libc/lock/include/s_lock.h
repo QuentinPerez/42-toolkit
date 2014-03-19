f5e30778e36a63d599507cd194cd7f6a0af4b4ad
Quentin PEREZ <qperez42@gmail.com>
2014-03-19 15:37:26 +0100

    [LOCK]
    add copyright

--- srcs/libc/lock/include/s_lock.h
+++ srcs/libc/lock/include/s_lock.h
@@ -6,9 +6,29 @@
 /*   By: qperez <qperez42@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/02 17:21:07 by qperez            #+#    #+#             */
-/*   Updated: 2014/03/19 15:05:53 by qperez           ###   ########.fr       */
+/*   Updated: 2014/03/19 15:36:32 by qperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+** <This file contains s_lock prototype>
+** Copyright (C) <2013>  Quentin Perez <qperez42@gmail.com>
+**
+** This file is part of 42-toolkit.
+**
+** 42-toolkit is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef S_LOCK_H
 # define S_LOCK_H
--- srcs/libc/lock/src/s_lock.c
+++ srcs/libc/lock/src/s_lock.c
@@ -6,9 +6,30 @@
 /*   By: qperez <qperez42@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/02 17:20:45 by qperez            #+#    #+#             */
-/*   Updated: 2014/03/19 15:06:37 by qperez           ###   ########.fr       */
+/*   Updated: 2014/03/19 15:37:20 by qperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+** <This file contains s_lock method>
+** < init, destroy, lock, release >
+** Copyright (C) <2013>  Quentin Perez <qperez42@gmail.com>
+**
+** This file is part of 42-toolkit.
+**
+** 42-toolkit is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include <lock/s_lock.h>
 #include <f_error/m_error.h>
@@ -29,15 +50,15 @@ bool	f_lock_lock(t_lock *v_this, void **data)
 	return (true);
 }
 
-bool	f_lock_destroy(t_lock *v_this)
-{
-	return (!(pthread_mutex_destroy(&v_this->lock) != 0));
-}
-
 bool	f_lock_release(t_lock *v_this, void **data)
 {
 	if (pthread_mutex_unlock(&v_this->lock) != 0)
 		return (M_ERROR(false, "Fail unlock"));
 	*data = NULL;
 	return (true);
+}
+
+bool	f_lock_destroy(t_lock *v_this)
+{
+	return (!(pthread_mutex_destroy(&v_this->lock) != 0));
 }
