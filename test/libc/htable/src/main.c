c836bd26843286240bbd23fec18f33cf928f9b40
Quentin Perez <qperez@ocs.online.net>
2016-01-26 18:41:15 +0100

    [TEST]
    Add cmocka for htable

--- test/libc/htable/src/main.c
+++ test/libc/htable/src/main.c
@@ -11,68 +11,78 @@
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdarg.h>
+#include <setjmp.h>
+#include <cmocka.h>
 #include <htable/s_htable.h>
-#include <unit/s_unit.h>
-#include <time.h>
+#include <stdio.h>
 
-void	D_UNIT_FUNCT(ret_val)
-{
-	t_htable		htable;
-	char			str[6];
-	unsigned int	i;
+static void
+erase(void **state) {
+	t_htable	htable;
 
-	i = 0;
-	str[5] = 0;
-	F_UNIT_ASSERT(D_HTABLE(init)(&htable, 613, 0, free) == 1);
-	while (i < 100)
-	{
-		str[0] = rand() % 200;
-		str[1] = rand() % 175;
-		str[2] = rand() % 100;
-		str[3] = rand() % 125;
-		str[4] = rand() % 34;
-		i = i + 1;
-		F_UNIT_ASSERT(D_HTABLE(add)(&htable, str, malloc(sizeof(int))) == 1);
-	}
+	assert_true(D_HTABLE(init)(&htable, 997, NULL, NULL));
+	assert_true(D_HTABLE(add)(&htable, "key", (void*)(uintptr_t)1));
+	assert_int_equal((uintptr_t)D_HTABLE(erase)(&htable, "key"), 1);
+	assert_null(D_HTABLE(get)(&htable, "key"));
 	D_HTABLE(destroy)(&htable);
+	(void)state;
 }
 
-void	D_UNIT_FUNCT(memleaks)
-{
-	t_htable		htable;
-	char			str[6];
-	unsigned int	i;
+static void
+delete(void **state) {
+	t_htable	htable;
 
-	i = 0;
-	srand(time(NULL));
-	D_HTABLE(init)(&htable, 197, 0, free);
-	str[5] = 0;
-	while (i < 100)
-	{
-		str[0] = rand() % 200;
-		str[1] = rand() % 175;
-		str[2] = rand() % 100;
-		str[3] = rand() % 125;
-		str[4] = rand() % 34;
-		i = i + 1;
-		D_HTABLE(add)(&htable, str, malloc(sizeof(int)));
-	}
+	assert_true(D_HTABLE(init)(&htable, 997, NULL, NULL));
+	assert_true(D_HTABLE(add)(&htable, "key", (void*)(uintptr_t)1));
+	D_HTABLE(delete)(&htable, "key");
+	assert_null(D_HTABLE(get)(&htable, "key"));
 	D_HTABLE(destroy)(&htable);
-	(void)t;
+	(void)state;
 }
 
-int		main(int argc, char const** argv)
-{
-	t_unit	unit;
+static void
+get(void **state) {
+	t_htable	htable;
 
-	D_UNIT(init)(&unit);
-	D_UNIT(add_context)(&unit, "Leaks", 0, 0);
-	F_UNIT_ADD_TEST(&unit, "Leaks", memleaks);
-	D_UNIT(add_context)(&unit, "Method", 0, 0);
-	F_UNIT_ADD_TEST(&unit, "Method", ret_val);
-	D_UNIT(console_run)(&unit);
-	D_UNIT(destroy)(&unit);
+	assert_true(D_HTABLE(init)(&htable, 997, NULL, NULL));
+	assert_true(D_HTABLE(add)(&htable, "key", (void*)(uintptr_t)1));
+	assert_int_equal((uintptr_t)D_HTABLE(get)(&htable, "key"), 1);
+
+	D_HTABLE(destroy)(&htable);
+	(void)state;
+}
+
+static void
+add(void **state) {
+	t_htable	htable;
+
+	assert_true(D_HTABLE(init)(&htable, 997, NULL, NULL));
+	assert_true(D_HTABLE(add)(&htable, "key", (void*)(uintptr_t)1));
+	D_HTABLE(destroy)(&htable);
+	(void)state;
+}
+
+static void
+init(void **state) {
+	t_htable	htable;
+
+	assert_true(D_HTABLE(init)(&htable, 997, NULL, NULL));
+	D_HTABLE(destroy)(&htable);
+	(void)state;
+}
+
+int
+main(int argc, char const** argv) {
+	const struct CMUnitTest	test[] = {
+		cmocka_unit_test(init),
+		cmocka_unit_test(add),
+		cmocka_unit_test(get),
+		cmocka_unit_test(delete),
+		cmocka_unit_test(erase),
+	};
+
 	(void)argc;
 	(void)argv;
-	return (0);
+	return (cmocka_run_group_tests(test, 0, 0));
 }
